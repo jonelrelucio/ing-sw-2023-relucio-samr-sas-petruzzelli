561be03fa0a138d66df218e70e5c28927fec02ad
LucianSasPolimi <lucian.sas@mail.polimi.it>
2023-03-13 16:41:47 +0100

    Primo commit dello scheletro delle classi

--- src/main/java/org/example/models/CommonGoalCard.java
+++ src/main/java/org/example/models/CommonGoalCard.java
@@ -1,4 +1,4 @@
 package org.example.models;
 
-public class CommonGoalCard {
+public interface CommonGoalCard {
 }
--- src/main/java/org/example/models/Player.java
+++ src/main/java/org/example/models/Player.java
@@ -24,7 +24,7 @@ public class Player {
     }
 
     public void getEndGameToken(){
-        //?????
+        //????
     }
 
 
