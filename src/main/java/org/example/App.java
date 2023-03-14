52e8310525c2023df9ef100d6a94d463f2d707dc
LucianSasPolimi <lucian.sas@mail.polimi.it>
2023-03-14 16:31:22 +0100

    Cambio del main una seconda volta

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! cambiato da Lucian" );
+        System.out.println( "Hello World! cambiato da Lucian una seconda volta" );
     }
 }
