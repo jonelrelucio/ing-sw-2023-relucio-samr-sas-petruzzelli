73be06adeb79fc908d9bd98aca6649925f9ba613
hojo <jonel.relucio@mail.polimi.it>
2023-03-14 16:19:32 +0100

    prova

---
+++ src/main/java/org/example/models/CircularArrayList.java
@@ -0,0 +1,8 @@
+package org.example.models;
+
+import java.util.ArrayList;
+
+public class CircularArrayList extends ArrayList {
+
+
+}
---
+++ src/main/java/org/example/models/GameModel.java
@@ -0,0 +1,12 @@
+package org.example.models;
+
+public class GameModel {
+    private int numOfPlayer;
+    private CircularArrayList<Player> playerList;
+    private CommonGoalCard commonGoalCard;
+    private Board board;
+    pritate State state;
+    private Round round;
+
+
+}
