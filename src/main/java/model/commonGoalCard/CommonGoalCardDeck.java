57fdf151d0860e9da94e1935d8453e0eaca6755b
hojo <jonel.relucio@mail.polimi.it>
2023-03-23 12:37:45 +0100

    Update CommonGoalCardDeck

--- src/main/java/model/commonGoalCard/CommonGoalCardDeck.java
+++ src/main/java/model/commonGoalCard/CommonGoalCardDeck.java
@@ -5,8 +5,30 @@ import java.util.Stack;
 
 public class CommonGoalCardDeck {
     private HashMap<CommonGoalCard, Stack<Integer>> deck;
-    public int draw() {
-        return 0;
+
+    private static int[][] scoringTokenArray = {
+            {4, 8},
+            {4, 6, 8},
+            {2, 4, 6, 8}
+    };
+
+    private Stack<Integer> buildScoringStack(int key){
+        Stack<Integer> scoringTokenStack = new Stack<>();
+        for (int i = 0; i < scoringTokenArray[key-2].length; i++){
+            scoringTokenStack.push(scoringTokenArray[key-1][i]);
+        }
+        return scoringTokenStack;
     }
+
+    public CommonGoalCardDeck(int numOfPlayers, CommonGoalCard card1, CommonGoalCard card2) {
+        deck.put(card1, buildScoringStack(numOfPlayers));
+        deck.put(card2, buildScoringStack(numOfPlayers));
+    }
+
+    public HashMap<CommonGoalCard, Stack<Integer>> getDeck() {
+        return deck;
+    }
+
+
 
 }
