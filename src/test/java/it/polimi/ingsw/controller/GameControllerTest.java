c26b7d65988e13d2e0a75354a061dfbe7e79e998
jonelrelucio <jonel.relucio@mail.polimi.it>
2023-04-04 02:31:57 +0200

    Update Tests

--- src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/GameControllerTest.java
@@ -8,6 +8,7 @@ import it.polimi.ingsw.model.commonGoalCard.CommonGoalCard1;
 import it.polimi.ingsw.model.commonGoalCard.CommonGoalCard2;
 import it.polimi.ingsw.model.commonGoalCard.CommonGoalCardDeck;
 import it.polimi.ingsw.model.factory.PersonalGoalCardBag;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameControllerTest {
@@ -20,7 +21,7 @@ public class GameControllerTest {
                         {new ItemTile(ItemTileType.EMPTY),  new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.FRAME),   new ItemTile(ItemTileType.PLANT)},
                         {new ItemTile(ItemTileType.CAT),    new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.GAME),    new ItemTile(ItemTileType.GAME)}};
 
-        private final ItemTile[][] bookshelf7 =
+        private final ItemTile[][] bookshelf2 =
                 {       {new ItemTile(ItemTileType.FRAME),  new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.FRAME),   new ItemTile(ItemTileType.TROPHY),  new ItemTile(ItemTileType.TROPHY)},
                         {new ItemTile(ItemTileType.BOOK),   new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.PLANT),   new ItemTile(ItemTileType.TROPHY),  new ItemTile(ItemTileType.TROPHY)},
                         {new ItemTile(ItemTileType.BOOK),   new ItemTile(ItemTileType.BOOK),    new ItemTile(ItemTileType.FRAME),   new ItemTile(ItemTileType.FRAME),   new ItemTile(ItemTileType.FRAME)},
@@ -29,7 +30,7 @@ public class GameControllerTest {
                         {new ItemTile(ItemTileType.FRAME),  new ItemTile(ItemTileType.CAT),     new ItemTile(ItemTileType.CAT),     new ItemTile(ItemTileType.PLANT),   new ItemTile(ItemTileType.FRAME)}};
 
         @Test
-        public void checkUpdatePlayerPoints1() {
+        public void testUpdatePlayerPoints1() {
                 int numOfPlayers = 2;
                 gameModel = new GameModel(numOfPlayers);
                 gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
@@ -41,16 +42,119 @@ public class GameControllerTest {
         }
 
         @Test
-        public void checkUpdatePlayerPointsWinner() {
+        public void testUpdatePlayerPointsWinner() {
                 int numOfPlayers = 2;
                 gameModel = new GameModel(3);
-                CommonGoalCardDeck deck = new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2());
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
                 gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
                 gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf1);
                 gameModel.getCurrentPlayer().setWinner();
                 GameController gc = new GameController(gameModel);
                 gc.updateCurrentPlayerScore();
                 System.out.println(gameModel.getCurrentPlayer().getScore());
+        }
+
+        @Test
+        public void testLoop() {
+                int numOfPlayers = 2;
+                gameModel = new GameModel(numOfPlayers);
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
+                gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
+                gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf1);
+                GameController gc = new GameController(gameModel);
+                gc.updateCurrentPlayerScore();
+                boolean flag = true;
+                int points = gameModel.getCurrentPlayer().getScore();
+                for (int i = 0; i < 4; i++ ){
+                        gc.updateCurrentPlayerScore();
+                        if (points != gameModel.getCurrentPlayer().getScore()) flag = false;
+                        points = gameModel.getCurrentPlayer().getScore();
+                }
+                Assertions.assertTrue(flag);
+        }
+
+        @Test
+        public void testLoopAndWinner() {
+                int numOfPlayers = 2;
+                gameModel = new GameModel(numOfPlayers);
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
+                gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
+                gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf1);
+                gameModel.getCurrentPlayer().setWinner();
+                GameController gc = new GameController(gameModel);
+                gc.updateCurrentPlayerScore();
+                boolean flag = true;
+                int points = gameModel.getCurrentPlayer().getScore();
+                for (int i = 0; i < 4; i++) {
+                        gc.updateCurrentPlayerScore();
+                        if (points != gameModel.getCurrentPlayer().getScore()) flag = false;
+                        points = gameModel.getCurrentPlayer().getScore();
+                }
+
+        }
+
+        @Test
+        public void testUpdatePlayerPoints2() {
+                int numOfPlayers = 2;
+                gameModel = new GameModel(numOfPlayers);
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
+                gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
+                gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf2);
+                GameController gc = new GameController(gameModel);
+                gc.updateCurrentPlayerScore();
+                System.out.println(gameModel.getCurrentPlayer().getScore());
+        }
+
+        @Test
+        public void testUpdatePlayerPointsWinner2() {
+                int numOfPlayers = 2;
+                gameModel = new GameModel(3);
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
+                gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
+                gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf2);
+                gameModel.getCurrentPlayer().setWinner();
+                GameController gc = new GameController(gameModel);
+                gc.updateCurrentPlayerScore();
+                System.out.println(gameModel.getCurrentPlayer().getScore());
+        }
+
+        @Test
+        public void testLoop2() {
+                int numOfPlayers = 2;
+                gameModel = new GameModel(numOfPlayers);
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
+                gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
+                gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf2);
+                GameController gc = new GameController(gameModel);
+                gc.updateCurrentPlayerScore();
+                boolean flag = true;
+                int points = gameModel.getCurrentPlayer().getScore();
+                for (int i = 0; i < 4; i++ ){
+                        gc.updateCurrentPlayerScore();
+                        if (points != gameModel.getCurrentPlayer().getScore()) flag = false;
+                        points = gameModel.getCurrentPlayer().getScore();
+                }
+                Assertions.assertTrue(flag);
+        }
+
+        @Test
+        public void testLoopAndWinner2() {
+                int numOfPlayers = 2;
+                gameModel = new GameModel(numOfPlayers);
+                gameModel.setCommonGoalCardDeck(new CommonGoalCardDeck(numOfPlayers, new CommonGoalCard1(), new CommonGoalCard2()));
+                gameModel.setCurrentPlayer(new Player("Alessandro", PersonalGoalCardBag.drawPersonalGoalCard(numOfPlayers, 2)));
+                gameModel.getCurrentPlayer().getBookshelf().setBookshelfMatrix(bookshelf2);
+                gameModel.getCurrentPlayer().setWinner();
+                GameController gc = new GameController(gameModel);
+                gc.updateCurrentPlayerScore();
+                boolean flag = true;
+                int points = gameModel.getCurrentPlayer().getScore();
+                for (int i = 0; i < 4; i++) {
+                        gc.updateCurrentPlayerScore();
+                        if (points != gameModel.getCurrentPlayer().getScore()) flag = false;
+                        points = gameModel.getCurrentPlayer().getScore();
+                }
+
         }
 
 
