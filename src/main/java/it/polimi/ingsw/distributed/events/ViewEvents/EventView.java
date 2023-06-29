c84a99f6ffb067cda5a8381e9874be7d5489fb9b
AlessandroPetruzzelli <alessandro.petruzzelli@mail.polimi.it>
2023-06-29 11:09:36 +0200

    added javadoc

--- src/main/java/it/polimi/ingsw/distributed/events/ViewEvents/EventView.java
+++ src/main/java/it/polimi/ingsw/distributed/events/ViewEvents/EventView.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.distributed.events.ViewEvents;
 
+/**
+ * This enum represents all the events that the client could be notified of and has to handle
+ */
 public enum EventView {
     SELECT_COORDINATES_SUCCESS,
     SELECT_COORDINATES_FAIL,
@@ -9,6 +12,9 @@ public enum EventView {
     NEW_ORDER_SUCCESS,
     NEW_ORDER_FAIL,
     SELECT_COLUMN_FAIL,
-    END_GAME, NEW_TURN,
-    UPDATE_CHAT, SHOW_LAST_MESSAGES, UPDATE_PRIVATE_CHAT
+    END_GAME,
+    NEW_TURN,
+    UPDATE_CHAT,
+    SHOW_LAST_MESSAGES,
+    UPDATE_PRIVATE_CHAT
 }
--- src/main/java/it/polimi/ingsw/distributed/events/controllerEvents/EventController.java
+++ src/main/java/it/polimi/ingsw/distributed/events/controllerEvents/EventController.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.distributed.events.controllerEvents;
 
+/**
+ * This enum represents all the events that the controller could be notified of and has to handle
+ */
 public enum EventController {
     SELECT_COORDINATES,
     DESELECT_COORDINATES,
