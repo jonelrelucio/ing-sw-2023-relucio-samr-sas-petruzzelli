1c2cf238907470bf6f126a57a22065b74729c55c
hojo <jonel.relucio@mail.polimi.it>
2023-06-29 15:41:36 +0200

    Updated ItemTileTest

--- src/test/java/it/polimi/ingsw/server/model/ItemTile/ItemTileTest.java
+++ src/test/java/it/polimi/ingsw/server/model/ItemTile/ItemTileTest.java
@@ -7,7 +7,28 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ItemTileTest {
 
     @Test
-    void testSettersAndGerres() {
+    void testSettersAndGetters() {
+        ItemTileType itemType = ItemTileType.CAT;
+        ItemTile item = new ItemTile(itemType);
+        assertEquals(itemType, item.getItemTileType());
+
+        itemType = ItemTileType.BOOK;
+        item.setItemTileType(itemType);
+        assertEquals(itemType, item.getItemTileType());
+
+        int itemId = 2;
+        item.setId(itemId);
+        assertEquals(itemId, item.getId());
+
+        itemType = ItemTileType.EMPTY;
+        item.setItemTileType(itemType);
+        assertTrue(item.isEmpty());
+
+        itemType = ItemTileType.BOOK;
+        item.setItemTileType(itemType);
+        ItemTileType itemType2 = ItemTileType.BOOK;
+        ItemTile item2 = new ItemTile(itemType2);
+        assertTrue(item.equals(item2));
 
     }
 }
