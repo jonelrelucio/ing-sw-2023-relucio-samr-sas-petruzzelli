a41ac8ef8b8734a685c867b55312d08fa66a0d8e
jonelrelucio <jonel.relucio@mail.polimi.it>
2023-06-30 04:19:50 +0200

    updated javadoc

--- src/main/java/it/polimi/ingsw/server/model/util/CircularArrayList.java
+++ src/main/java/it/polimi/ingsw/server/model/util/CircularArrayList.java
@@ -4,12 +4,27 @@ import it.polimi.ingsw.server.model.Player;
 
 import java.util.ArrayList;
 
+/**
+ * The circular array list extends the array list
+ * @param <E> Generic element of the Circular Array List
+ */
 public class CircularArrayList<E> extends ArrayList<E> {
+
+    /**
+     * Gets the Element on a certain index position in the list
+     * @param index index of the element to return
+     * @return      the element
+     */
     @Override
     public E get(int index) {
         return super.get(index % size());
     }
 
+    /**
+     * Checks if the circular array lisit contains an object
+     * @param o element whose presence in this list is to be tested
+     * @return  true if the element is in the list
+     */
     @Override
     public boolean contains(Object o){
         if (o == null ) throw new NullPointerException("Nickname string is null");
